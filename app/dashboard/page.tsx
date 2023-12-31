2029433b4ebcd409dea8c68ab2b2daafdd0c9318
Roohan Khan <roohank8@gmail.com>
2024-01-01 01:35:38 +0500

    .

--- app/dashboard/page.tsx
+++
@@ -1,5 +0,0 @@
-const page = () => {
-  return <div>Dasboard</div>;
-};
-
-export default page;
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,4 +1,5 @@
 import { sql } from '@vercel/postgres';
+import { unstable_noStore as noStore } from 'next/cache';
 import {
   CustomerField,
   CustomersTableType,
@@ -18,8 +19,8 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
@@ -30,6 +31,7 @@ export async function fetchRevenue() {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch revenue data.');
   }
+  noStore()
 }
 
 export async function fetchLatestInvoices() {
@@ -50,6 +52,7 @@ export async function fetchLatestInvoices() {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch the latest invoices.');
   }
+  noStore()
 }
 
 export async function fetchCardData() {
@@ -85,6 +88,7 @@ export async function fetchCardData() {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch card data.');
   }
+  noStore()
 }
 
 const ITEMS_PER_PAGE = 6;
@@ -121,6 +125,7 @@ export async function fetchFilteredInvoices(
     console.error('Database Error:', error);
     throw new Error('Failed to fetch invoices.');
   }
+  noStore()
 }
 
 export async function fetchInvoicesPages(query: string) {
@@ -142,6 +147,7 @@ export async function fetchInvoicesPages(query: string) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch total number of invoices.');
   }
+  noStore()
 }
 
 export async function fetchInvoiceById(id: string) {
@@ -218,6 +224,7 @@ export async function fetchFilteredCustomers(query: string) {
     console.error('Database Error:', err);
     throw new Error('Failed to fetch customer table.');
   }
+  noStore()
 }
 
 export async function getUser(email: string) {
