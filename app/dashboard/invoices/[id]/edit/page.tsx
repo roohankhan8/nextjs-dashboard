4f77e0e6899077642dc2c595fa57b5bafeca8e8b
Roohan Khan <roohank8@gmail.com>
2024-01-07 00:57:38 +0500

    .

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -3,14 +3,19 @@ import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 
 import { notFound } from 'next/navigation';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Edit Invoice',
+};
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
-  const invoice = await fetchInvoiceById(id)
-    const customers = await fetchCustomers()
-//   const [invoice, customers] = await Promise.all([
-//     fetchInvoiceById(id),
-//     fetchCustomers(),
-//   ]);
+  const invoice = await fetchInvoiceById(id);
+  const customers = await fetchCustomers();
+  //   const [invoice, customers] = await Promise.all([
+  //     fetchInvoiceById(id),
+  //     fetchCustomers(),
+  //   ]);
   if (!invoice) {
     notFound();
   }
